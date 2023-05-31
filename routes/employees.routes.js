771dc2d5dc23d880da8a41fb09fb13feb17b3e9e
Cyprian Adamski <cyprian.adamski@gmail.com>
2023-05-31 11:38:27 +0200

    add mongoose

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,64 +1,121 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+// const db = require('./../db');
 const ObjectId = require('mongodb').ObjectId;
+const Employee = require('../models/employee.model');
 
 // router.get('/employees', (req, res) => {
 //   res.json(db.employees);
 // });
-router.get('/employees', (req, res) => {
-  req.db
-    .collection('employees')
-    .find()
-    .toArray((err, data) => {
-      if (err) res.status(500).json({ message: err });
-      else res.json(data);
-    });
+// router.get('/employees', (req, res) => {
+//   req.db
+//     .collection('employees')
+//     .find()
+//     .toArray((err, data) => {
+//       if (err) res.status(500).json({ message: err });
+//       else res.json(data);
+//     });
+// });
+
+router.get('/employees', async (req, res) => {
+  try {
+    res.json(await Employee.find());
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
+
+/////////////////////////***********************////////////////////////////////
 
 // router.get('/employees/random', (req, res) => {
 //   res.json(db.employees[Math.floor(Math.random() * db.length)]);
 // });
-router.get('/employees/random', (req, res) => {
-  req.db
-    .collection('employees')
-    .aggregate([{ $sample: { size: 1 } }])
-    .toArray((err, data) => {
-      if (err) res.status(500).json({ message: err });
-      else res.json(data[0]);
-    });
+
+// router.get('/employees/random', (req, res) => {
+//   req.db
+//     .collection('employees')
+//     .aggregate([{ $sample: { size: 1 } }])
+//     .toArray((err, data) => {
+//       if (err) res.status(500).json({ message: err });
+//       else res.json(data[0]);
+//     });
+// });
+
+router.get('/employees/random', async (req, res) => {
+  try {
+    const count = await Employee.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const empl = await Employee.findOne().skip(rand);
+    if (!empl) res.status(404).json({ message: 'Not found' });
+    else res.json(empl);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
+
+/////////////////////////***********************////////////////////////////////
 
 // router.get('/employees/:id', (req, res) => {
 //   res.json(db.employees.find((item) => item.id == req.params.id));
 // });
-router.get('/employees/:id', (req, res) => {
-  req.db
-    .collection('employees')
-    .findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
-      if (err) res.status(500).json({ message: err });
-      else if (!data) res.status(404).json({ message: 'Not found' });
-      else res.json(data);
-    });
+
+// router.get('/employees/:id', (req, res) => {
+//   req.db
+//     .collection('employees')
+//     .findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
+//       if (err) res.status(500).json({ message: err });
+//       else if (!data) res.status(404).json({ message: 'Not found' });
+//       else res.json(data);
+//     });
+// });
+
+router.get('/employees/:id', async (req, res) => {
+  try {
+    const empl = await Employee.findById(req.params.id);
+    if (!empl) res.status(404).json({ message: 'Not found' });
+    else res.json(empl);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
+
+/////////////////////////***********************////////////////////////////////
 
 // router.post('/employees', (req, res) => {
 //   const { firstName, lastName } = req.body;
 //   db.employees.push({ id: 3, firstName, lastName });
 //   res.json({ message: 'OK' });
 // });
-router.post('/employees', (req, res) => {
-  const { firstName, lastName, department } = req.body;
-  req.db
-    .collection('employees')
-    .insertOne(
-      { firstName: firstName, lastName: lastName, department: department },
-      (err, data) => {
-        if (err) res.status(500).json({ message: err });
-        else res.json({ message: 'OK' });
-      }
-    );
+
+// router.post('/employees', (req, res) => {
+//   const { firstName, lastName, department } = req.body;
+//   req.db
+//     .collection('employees')
+//     .insertOne(
+//       { firstName: firstName, lastName: lastName, department: department },
+//       (err, data) => {
+//         if (err) res.status(500).json({ message: err });
+//         else res.json({ message: 'OK' });
+//       }
+//     );
+// });
+
+router.post('/employees', async (req, res) => {
+  try {
+    const { firstName, lastName, department } = req.body;
+    const newEmployee = new Employee({
+      firstName: firstName,
+      lastName: lastName,
+      department: department,
+    });
+    await newEmployee.save();
+    res.json({ message: 'OK' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
+
+/////////////////////////***********************////////////////////////////////
 
 // router.put('/employees/:id', (req, res) => {
 //   const { firstName, lastName } = req.body;
@@ -67,36 +124,77 @@ router.post('/employees', (req, res) => {
 //   );
 //   res.json({ message: 'OK' });
 // });
-router.put('/employees/:id', (req, res) => {
+
+// router.put('/employees/:id', (req, res) => {
+//   const { firstName, lastName, department } = req.body;
+
+//   req.db.collection('employees').updateOne(
+//     { _id: ObjectId(req.params.id) },
+//     {
+//       $set: {
+//         firstName: firstName,
+//         lastName: lastName,
+//         department: department,
+//       },
+//     },
+//     (err) => {
+//       if (err) res.status(500).json({ message: err });
+//       else res.json({ message: 'OK' });
+//     }
+//   );
+// });
+
+router.put('/employees/:id', async (req, res) => {
   const { firstName, lastName, department } = req.body;
 
-  req.db.collection('employees').updateOne(
-    { _id: ObjectId(req.params.id) },
-    {
-      $set: {
-        firstName: firstName,
-        lastName: lastName,
-        department: department,
-      },
-    },
-    (err) => {
-      if (err) res.status(500).json({ message: err });
-      else res.json({ message: 'OK' });
+  try {
+    const empl = await Employee.findById(req.params.id);
+    if (empl) {
+      await Employee.updateOne(
+        { _id: req.params.id },
+        {
+          $set: {
+            firstName: firstName,
+            lastName: lastName,
+            department: department,
+          },
+        }
+      );
+      res.json({ message: 'OK' });
+    } else {
+      res.status(404).json({ message: 'Not found...' });
     }
-  );
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
+
+/////////////////////////***********************////////////////////////////////
 
 // router.delete('/employees/:id', (req, res) => {
 //   db = db.employees.filter((item) => item.id != req.params.id);
 //   res.json({ message: 'OK' });
 // });
-router.delete('/employees/:id', (req, res) => {
-  req.db
-    .collection('employees')
-    .deleteOne({ _id: ObjectId(req.params.id) }, (err) => {
-      if (err) res.status(500).json({ message: err });
-      else res.json({ message: 'OK' });
-    });
+
+// router.delete('/employees/:id', (req, res) => {
+//   req.db
+//     .collection('employees')
+//     .deleteOne({ _id: ObjectId(req.params.id) }, (err) => {
+//       if (err) res.status(500).json({ message: err });
+//       else res.json({ message: 'OK' });
+//     });
+// });
+
+router.delete('/employees/:id', async (req, res) => {
+  try {
+    const empl = await Employee.findById(req.params.id);
+    if (empl) {
+      await Employee.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,55 +1,72 @@
 const express = require('express');
 const cors = require('cors');
-const mongoClient = require('mongodb').MongoClient;
+// const mongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
-mongoClient.connect(
-  'mongodb://localhost:27017',
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Successfully connected to the database');
-      const db = client.db('companyDB');
-      const app = express();
-      // db.collection('employees').find({ department: 'IT' }, (err, data) => {
-      //   if (!err) console.log(data);
-      // });
-      // db.collection('employees')
-      //   .find({ department: 'IT' })
-      //   .toArray((err, data) => {
-      //     if (!err) {
-      //       console.log(data);
-      //     }
-      //   });
-      // db.collection('departments').insertOne({ name: 'Management' }, (err) => {
-      //   if (err) console.log('err');
-      // });
+const app = express();
 
-      app.use(cors());
-      app.use(express.json());
-      app.use(express.urlencoded({ extended: false }));
+// mongoClient.connect(
+//   'mongodb://localhost:27017',
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (err, client) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log('Successfully connected to the database');
+//       const db = client.db('companyDB');
+//       const app = express();
 
-      app.use((req, res, next) => {
-        req.db = db;
-        next();
-      });
+////////////////////////////////////
 
-      app.use('/api', employeesRoutes);
-      app.use('/api', departmentsRoutes);
-      app.use('/api', productsRoutes);
+// db.collection('employees').find({ department: 'IT' }, (err, data) => {
+//   if (!err) console.log(data);
+// });
+// db.collection('employees')
+//   .find({ department: 'IT' })
+//   .toArray((err, data) => {
+//     if (!err) {
+//       console.log(data);
+//     }
+//   });
+// db.collection('departments').insertOne({ name: 'Management' }, (err) => {
+//   if (err) console.log('err');
+// });
 
-      app.use((req, res) => {
-        res.status(404).send({ message: 'Not found...' });
-      });
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-      app.listen('8000', () => {
-        console.log('Server is running on port: 8000');
-      });
-    }
-  }
-);
+// app.use((req, res, next) => {
+//   req.db = db;
+//   next();
+// });
+
+app.use('/api', employeesRoutes);
+app.use('/api', departmentsRoutes);
+app.use('/api', productsRoutes);
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+// connects our backend code with the database
+mongoose.connect('mongodb://localhost:27017/companyDB', {
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', (err) => console.log('Error ' + err));
+
+app.listen('8000', () => {
+  console.log('Server is running on port: 8000');
+});
+//     }
+//   }
+// );
